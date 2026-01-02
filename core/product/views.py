91f7d27b24de3a278c2ea89f69496a6af8cc2a6e
Arian <153840222+mhmdrezachizari@users.noreply.github.com>
2026-01-02 16:47:21 +0330

    s6

--- core/product/views.py
+++ core/product/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from .models import Product
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Product
 from .serializers import ProductSerializer
 # Create your views here.
 
 class ProductView(APIView):
     def get(self, request):
         products = Product.objects.all()
+        # print(products)
         ser_data = ProductSerializer(products, many=True)
-        return Response(ser_data.data)
+        return Response(ser_data.data , status=status.HTTP_200_OK)
